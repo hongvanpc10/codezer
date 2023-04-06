3841d3087c28dc1a9fad7f338c691a48deb425fd
hongvanpc10 <hongvanpc10@gmail.com>
2023-04-06 18:43:52 +0700

    fvvrrf

--- components/fly.tsx
+++ components/fly.tsx
@@ -1,13 +1,5 @@
 import Script from 'next/script'
 
 export default function Fly() {
-	return (
-		<Script
-			src='https://anonyviet.com/resource/ruoibay/ruoi.js'
-			type='text/javascript'
-			var_1='true'
-			var_2='false'
-			var_3='false'
-		></Script>
-	)
+	return <Script src='/fly.js' type='text/javascript'></Script>
 }
