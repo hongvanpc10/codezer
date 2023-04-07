94e466bafa1ce5542b0f79e2ddfcf39cfd723198
hongvanpc10 <hongvanpc10@gmail.com>
2023-04-07 12:39:14 +0700

    hvfhv

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -74,23 +74,34 @@ const Community: NextPageWithLayout = () => {
 						</div>
 					</div>
 
-					<p className='px-2 !prose !prose-blue prose-img:rounded-2xl prose-video:rounded-2xl prose-img:mx-auto prose-a:underline-offset-2 prose-p:break-words prose-headings:break-words prose-a:break-words prose-pre:!rounded-2xl prose-td:!p-3 even:prose-tr:bg-blue-50 prose-th:!p-3 prose-tr:rounded-xl prose-tr:border-none prose-thead:bg-blue-100/75 prose-thead:rounded-xl prose-thead:border-none prose-table:border-separate first:prose-th:rounded-l-xl last:prose-th:rounded-r-xl first:prose-td:rounded-l-xl last:prose-td:rounded-r-xl prose-table:border-spacing-px prose-figcaption:text-center prose-figcaption:italic prose-figcaption:!mt-3 prose-pre:scroll-sm prose-blockquote:!not-italic prose-blockquote:!font-normal prose-blockquote:bg-blue-50/50 [&>*]:prose-blockquote:before:hidden [&>*]:prose-blockquote:after:hidden prose-blockquote:py-1 first:[&>*]:prose-blockquote:!mt-2 last:[&>*]:prose-blockquote:!mb-2 prose-blockquote:pr-4 prose-blockquote:rounded-r-2xl prose-th:!align-middle mt-3 mb-4'>
+					<p className='px-2 !prose !prose-blue prose-img:rounded-2xl prose-video:rounded-2xl prose-img:mx-auto prose-a:underline-offset-2 prose-p:break-words prose-headings:break-words prose-a:break-words prose-pre:!rounded-2xl prose-td:!p-3 even:prose-tr:bg-blue-50 prose-th:!p-3 prose-tr:rounded-xl prose-tr:border-none prose-thead:bg-blue-100/75 prose-thead:rounded-xl prose-thead:border-none prose-table:border-separate first:prose-th:rounded-l-xl last:prose-th:rounded-r-xl first:prose-td:rounded-l-xl last:prose-td:rounded-r-xl prose-table:border-spacing-px prose-figcaption:text-center prose-figcaption:italic prose-figcaption:!mt-3 prose-pre:scroll-sm prose-blockquote:!not-italic prose-blockquote:!font-normal prose-blockquote:bg-blue-50/50 [&>*]:prose-blockquote:before:hidden [&>*]:prose-blockquote:after:hidden prose-blockquote:py-1 first:[&>*]:prose-blockquote:!mt-2 last:[&>*]:prose-blockquote:!mb-2 prose-blockquote:pr-4 prose-blockquote:rounded-r-2xl prose-th:!align-middle mt-3 mb-4 prose-p:!my-2'>
 						<p>
-							Ngoài là 1 Editor để code, VSCode còn là ứng dụng
-							gọi điện, share màn hình, điều khiển màn hình để
-							cùng pair programming, review code,... 🤡
+							Extension hỗ trợ nhận diện thụt đầu dòng trong
+							vscode
+						</p>
+						<p>------</p>
+						<p>
+							Thụt đầu dòng rất quan trọng trong các ngôn ngữ như
+							Python, Nim, Yaml,... Ngoài ra, nó giúp cho việc
+							format các đoạn code ở trong các ngôn ngữ không bắt
+							buộc cần phải có thụt đầu dòng dễ đọc hơn. Đặc biệt,
+							những cậu đang sử dụng VSCode thì mình đề xuất một
+							extension có tên {'"Indent-Rainbow"'}, có thể tạo
+							các vùng màu giúp phân biệt các đoạn code trong cùng
+							một khối lệnh.
 						</p>
 						<p>
-							<strong>Extension:</strong> Duckly: Pair Programming
-							with any IDE
+							Ngoài ra, extension này có thể sử dụng trên VSCode
+							bản web từ phiên bản v8.0.0.0.
 						</p>
+						<p>Hy vọng chia sẻ này có ích với cậu!</p>
 					</p>
 
 					<div>
 						<Image
 							alt=''
-							src='https://i.ibb.co/T2TBTT8/image.png'
-							ratio={[16, 9]}
+							src='https://i.ibb.co/V2ctWx2/image.png'
+							ratio={[16, 14]}
 							rounded='xl'
 						/>
 					</div>
@@ -109,85 +120,6 @@ const Community: NextPageWithLayout = () => {
 						</div>
 
 						<div>2 bình luận</div>
-					</div>
-
-					<div className='flex items-center border-y py-1.5 border-blue-900/5'>
-						<button className='flex items-center justify-center mx-1 rounded-xl transition hover:bg-slate-50 py-2 flex-1'>
-							<LikeIcon className='h-6' />
-						</button>
-						<button className='flex items-center justify-center mx-1 rounded-xl transition hover:bg-slate-50 py-2 flex-1'>
-							<SmsOutlineIcon className='h-6' />
-						</button>
-					</div>
-
-					<div className='flex items-center pt-3'>
-						<Avatar alt='' src={user.avatar} ringWidth={0} />
-						<form className='flex relative flex-1 items-center'>
-							<input
-								placeholder='Viết bình luận của bạn...'
-								className='w-full py-2 px-6 bg-blue-50/50 rounded-full ml-2 placeholder:text-blue-900/50'
-							/>
-
-							<div className='absolute flex items-center right-3 space-x-2 text-blue-900/60'>
-								<button>
-									<CameraOutlineIcon className='h-5' />
-								</button>
-								<button>
-									<SendIcon className='h-5' />
-								</button>
-							</div>
-						</form>
-					</div>
-				</div>
-
-				<div className='bg-white/90 rounded-3xl px-3 pt-6 pb-4 shadow-lg shadow-blue-900/5'>
-					<div className='flex items-center px-2'>
-						<Link href={''}>
-							<Avatar
-								src={images.logo}
-								alt=''
-								ringWidth={0}
-								size={9}
-							/>
-						</Link>
-
-						<div className='ml-3'>
-							<Link href={''}>
-								<h3 className='font-semibold leading-5 flex items-center'>
-									Codezer
-									<TickIcon className='h-[1.125rem] text-sky-500 ml-1' />
-								</h3>
-								<span className='text-sm'>1 ngày trước</span>
-							</Link>
-						</div>
-					</div>
-
-					<p className='px-2 !prose !prose-blue prose-img:rounded-2xl prose-video:rounded-2xl prose-img:mx-auto prose-a:underline-offset-2 prose-p:break-words prose-headings:break-words prose-a:break-words prose-pre:!rounded-2xl prose-td:!p-3 even:prose-tr:bg-blue-50 prose-th:!p-3 prose-tr:rounded-xl prose-tr:border-none prose-thead:bg-blue-100/75 prose-thead:rounded-xl prose-thead:border-none prose-table:border-separate first:prose-th:rounded-l-xl last:prose-th:rounded-r-xl first:prose-td:rounded-l-xl last:prose-td:rounded-r-xl prose-table:border-spacing-px prose-figcaption:text-center prose-figcaption:italic prose-figcaption:!mt-3 prose-pre:scroll-sm prose-blockquote:!not-italic prose-blockquote:!font-normal prose-blockquote:bg-blue-50/50 [&>*]:prose-blockquote:before:hidden [&>*]:prose-blockquote:after:hidden prose-blockquote:py-1 first:[&>*]:prose-blockquote:!mt-2 last:[&>*]:prose-blockquote:!mb-2 prose-blockquote:pr-4 prose-blockquote:rounded-r-2xl prose-th:!align-middle mt-3 mb-4'>
-						<p>{'Ngôn ngữ lập trình "nữa thật, nữa đùa" nhất:'}</p>
-					</p>
-
-					<div>
-						<Image
-							alt=''
-							src='https://i.ibb.co/WKDfHyz/image.png'
-							ratio={[1,1]}
-							rounded='xl'
-						/>
-					</div>
-
-					<div className='flex py-2 items-center justify-between text-sm px-2'>
-						<div className='last:[&_div_div]:hidden flex items-center'>
-							<FacebookCounter
-								counters={[
-									{ by: 'Hong Van', emoji: 'haha' },
-									{ by: 'Codezer', emoji: 'like' },
-								]}
-							/>
-
-							<span className='ml-2'>345</span>
-						</div>
-
-						<div>100 bình luận</div>
 					</div>
 
 					<div className='flex items-center border-y py-1.5 border-blue-900/5'>
