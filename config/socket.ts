e231e8380200912f2a9ae6557b459a994f763e0b
hongvanpc10 <hongvanpc10@gmail.com>
2023-04-17 19:16:35 +0700

    refreg

--- config/socket.ts
+++ config/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client'
 
 const socket = io(`${process.env.NEXT_PUBLIC_API_BASE_URL}`.slice(0, -4), {
-	withCredentials: true,
+	transports: ['polling', 'websocket'],
 })
 
 export default socket
