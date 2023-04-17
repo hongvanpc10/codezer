678dd3678e7cba238ce68a8850300ec568ba2e40
hongvanpc10 <hongvanpc10@gmail.com>
2023-04-17 19:38:36 +0700

    fdvdvg

--- config/socket.ts
+++ config/socket.ts
@@ -2,8 +2,6 @@ import { io } from 'socket.io-client'
 
 const socket = io(`${process.env.NEXT_PUBLIC_API_BASE_URL}`.slice(0, -4), {
 	autoConnect: false,
-	transports: ['websocket'],
-	secure: true,
 })
 
 export default socket
