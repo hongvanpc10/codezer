9ca2f9d3556ea7a354b0e7cea55c32adc44cd87a
hongvanpc10 <hongvanpc10@gmail.com>
2023-05-17 19:21:14 +0700

    rvebg

--- components/profile/main.tsx
+++ components/profile/main.tsx
@@ -46,7 +46,7 @@ export default function Main({ data }: { data: User }) {
 					<Tab key={index} className='flex-1'>
 						{({ selected }) => (
 							<div
-								className={`py-3 h-full flex items-center justify-center hover:bg-slate-50 rounded-2xl transition after:content-[""] after:absolute relative after:h-[0.1875rem] after:rounded-full after:transition-all after:left-1/2 after:-translate-x-1/2 after:-bottom-1.5 font-medium ${
+								className={`py-3 px-4 h-full flex items-center justify-center hover:bg-slate-50 rounded-2xl transition after:content-[""] after:absolute relative after:h-[0.1875rem] after:rounded-full after:transition-all after:left-1/2 after:-translate-x-1/2 after:-bottom-1.5 font-medium ${
 									selected
 										? 'after:bg-blue-500 text-blue-500 after:md:w-[60%] after:w-[80%]'
 										: 'after:md:w-[30%] after:w-[40%]'
