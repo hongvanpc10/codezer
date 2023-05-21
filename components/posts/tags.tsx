5017022a26ca7b094086749ca063edbb77656dd1
hongvanpc10 <hongvanpc10@gmail.com>
2023-05-21 11:03:12 +0700

    fbrth

--- components/posts/tags.tsx
+++ components/posts/tags.tsx
@@ -14,48 +14,50 @@ export default function Tags() {
 	const [limit, setLimit] = useState(true)
 
 	return (
-		<div className='bg-white max-w-2xl mx-auto max-h-[calc(100vh-9rem)] overflow-y-auto px-2 py-4 rounded-3xl shadow-xl shadow-blue-900/5'>
+		<div className='bg-white max-w-2xl mx-auto max-h-[calc(100vh-9rem)] px-2 py-4 rounded-3xl shadow-xl shadow-blue-900/5 flex flex-col'>
 			<h2 className='ml-4 text-lg font-medium'>Tags</h2>
 
-			<div className='mt-3 flex flex-wrap'>
-				{data &&
-					data
-						.slice(0, limit ? 10 : data.length)
-						.map((tag, index) => (
-							<Link
-								href={routes.postsByTag(tag.slice(1))}
-								key={index}
-								className='bg-blue-50/25 transition text-blue-900 hover:bg-blue-50 rounded-xl py-0.5 mb-3.5 mr-2 px-3'
-							>
-								{tag}
-							</Link>
+			<div className='flex-1 mt-3 overflow-y-auto scroll-xs'>
+				<div className='flex flex-wrap'>
+					{data &&
+						data
+							.slice(0, limit ? 10 : data.length)
+							.map((tag, index) => (
+								<Link
+									href={routes.postsByTag(tag.slice(1))}
+									key={index}
+									className='bg-blue-50/25 transition text-blue-900 hover:bg-blue-50 rounded-xl py-0.5 mb-3.5 mr-2 px-3'
+								>
+									{tag}
+								</Link>
+							))}
+
+					{isLoading &&
+						Array.from(Array(10)).map((_, i) => (
+							<div className='mb-3.5 mr-2' key={i}>
+								<Skeleton
+									height={7}
+									rounded='xl'
+									width={
+										[7, 7.5, 5, 6, 8, 9, 5, 7.5, 6, 8][i] +
+										'rem'
+									}
+								/>
+							</div>
 						))}
-
-				{isLoading &&
-					Array.from(Array(10)).map((_, i) => (
-						<div className='mb-3.5 mr-2' key={i}>
-							<Skeleton
-								height={7}
-								rounded='xl'
-								width={
-									[7, 7.5, 5, 6, 8, 9, 5, 7.5, 6, 8][i] +
-									'rem'
-								}
-							/>
-						</div>
-					))}
-			</div>
-
-			{data && data.length > 10 && (
-				<div className='flex justify-center'>
-					<button
-						className='text-blue-500 font-medium'
-						onClick={() => setLimit(!limit)}
-					>
-						{limit ? 'Xem tất cả' : 'Thu gọn'}
-					</button>
 				</div>
-			)}
+
+				{data && data.length > 10 && (
+					<div className='flex justify-center'>
+						<button
+							className='text-blue-500 font-medium'
+							onClick={() => setLimit(!limit)}
+						>
+							{limit ? 'Xem tất cả' : 'Thu gọn'}
+						</button>
+					</div>
+				)}
+			</div>
 		</div>
 	)
 }
